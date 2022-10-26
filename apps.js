85a74c6c407950be0f76a2e6b3a43f31db945fd1
hasselGOP <hassel.ortegapelaez@gmail.com>
2022-10-26 14:50:10 -0500

    agregando archivos

--- apps.js
+++ apps.js
@@ -1,7 +1,8 @@
-//  SIMULADOR INTERACTIVO DE LA TIENDA ONIXGLAM
+                                                                                                //  SIMULADOR INTERACTIVO DE LA TIENDA ONIXGLAM
 // Definir la cantidad que desea comprar 
 // Calcular el valor
 // Informar lo que pidió el usuario
+
 const menu = opcionSeleccionada => {
     switch (opcionSeleccionada) {
       case 1:
@@ -22,6 +23,8 @@ const menu = opcionSeleccionada => {
         alert("Ingresó una opcion invalida, Intentelo de nuevo");    
     }
   }
+
+
   //Funcion donde se le pide al usuario cuanta cantidad de productos desea
   const cuantos = (nombreDelProducto, precioDelProducto)  => {
      let cantidad = prompt(`¿Cuántas unidades del producto ${nombreDelProducto} desea?`)
@@ -29,9 +32,13 @@ const menu = opcionSeleccionada => {
     // informo lo que pidió
          alert(` El usuario-${nombre} seleccionó ${cantidad} ${nombreDelProducto}  y el total a pagar es $${precio}MXN`);
   } 
+
   //Variables
+
   let opcion;
+
   // PROGRAMA PRINCIPAL
+
   let nombre = prompt('Ingrese su usuario');
   const ingresarNombre = () => {
       while (nombre === "" || nombre === null  ) {
@@ -40,6 +47,7 @@ const menu = opcionSeleccionada => {
       alert(`Bienvenido ${nombre} a ONIXGLAM la tienda mas grande de LATINOAMERICA`);
   }
   ingresarNombre();
+
   // Pido opción a comprar
   opcion = parseInt(prompt("¿Qué deseas comprar? - Ingrese solamente un numero de las siguientes opciones:\n 1-Mouse \n 2-carcasa de smartphone \n3-Audifonos \n4-Teclado \n 0-FINALIZAR"));
   while (opcion !== 0) {  // mientras que la opcion sea diferente a o se le pregunta al usuario que elija de las opciones de los contrario el programa saldra
@@ -49,5 +57,5 @@ const menu = opcionSeleccionada => {
   }
   alert("Gracias por su visita!"); // cuando tecleo 0 el programa dara por finalizado y sacara un alert donde agradece su visita.
 
-// crear una funcion que calcule el costo total de la compra de lo seleccionado por el usuario y que le pregunte cuantos productos desea comprar y que le informe el total a pagar con impuestos incluidos
+
 
